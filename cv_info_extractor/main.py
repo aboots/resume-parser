8a367934ff80992d0723e9a3965dad0b080b2243
aboots <mahdi.abootorabi2@gmail.com>
2022-11-24 14:40:08 +0330

    feat: work on name part extraction

--- cv_info_extractor/main.py
+++ cv_info_extractor/main.py
@@ -9,8 +9,10 @@ def run(address, use_pos=True, output_file='output.txt'):
     for page in reader.pages:
         text = page.extract_text()
         final_text += text
-    print(final_text)
+    # print(final_text)
     normalizer = Normalizer()
     final_text = normalizer.normalize(final_text)
-    full_name = {NameDetection().find_name(final_text)}
-    print(f"Name is = {full_name}")
+    full_name, first_name, last_name = NameDetection().find_name(final_text)
+    print(f"Full Name is = {full_name}")
+    print(f"First Name is = {first_name}")
+    print(f"Last Name is = {last_name}")
--- cv_info_extractor/name_detector.py
+++ cv_info_extractor/name_detector.py
@@ -13,11 +13,12 @@ class NameDetection:
         file.close()
 
         self.pattern = f"(^|\W)(((({self.first_names_reg})(\W+))+({self.last_names_reg}))|({self.last_names_reg})|({self.first_names_reg}))($|\W)"
+        self.pattern2 = f"(^|\W)((((({self.first_names_reg})(\W+))+)({self.last_names_reg}))|({self.last_names_reg})|({self.first_names_reg}))($|\W)"
 
     def match_name(self, inp):
         matches = []
         for keyword_count in range(10, 0, -1):
-            count_pattern = self.pattern.format()
+            count_pattern = self.pattern2.format()
             for matched in re.finditer(count_pattern, inp):
                 start, end = matched.span()
                 inp = inp[:start] + '#' * (end - start) + inp[end:]
@@ -27,5 +28,8 @@ class NameDetection:
     def find_name(self, text):
         matched_names = self.match_name(text)
         if not matched_names:
-            return 'Not Found'
-        return matched_names[0].group().strip()
+            return ['Not Found'] * 3
+        full_name = matched_names[0].group().strip()
+        first_name = matched_names[0].groups()[3].strip() if matched_names[0].groups()[3] else 'None'
+        last_name = matched_names[0].groups()[-4].strip() if matched_names[0].groups()[-4] else 'None'
+        return full_name, first_name, last_name
