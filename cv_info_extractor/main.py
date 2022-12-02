629bc5c45842147a73df9f9aa9a56eadf6736809
Arshan <arshandalili@gmail.com>
2022-12-02 20:51:26 +0330

    Some modifications

--- cv_info_extractor/main.py
+++ cv_info_extractor/main.py
@@ -1,5 +1,6 @@
 from PyPDF2 import PdfReader
 from hazm import Normalizer
+import re
 
 from cv_info_extractor.email_detector import EmailDetection
 from cv_info_extractor.name_detector import NameDetection
@@ -8,23 +9,27 @@ from cv_info_extractor.date_extractor import DateDetection
 from cv_info_extractor.section_extractor import SectionExtractor
 
 
-def run(address, use_pos=True, output_file='output.txt'):
+def run(address):
     final_text = ''
+    result = {}
     reader = PdfReader(address)
     for page in reader.pages:
         text = page.extract_text()
         final_text += text
     email = EmailDetection().find_email(final_text)
-    print(f"Email = {email}")
     normalizer = Normalizer()
     final_text = normalizer.normalize(final_text)
+    final_text = re.sub(r'\s+', ' ', final_text)
+    result['ایمیل'] = email
     full_name, first_name, last_name = NameDetection().find_name(final_text)
-    print(f"Full Name = {full_name}")
-    print(f"First Name = {first_name}")
-    print(f"Last Name = {last_name}")
+    result['نام'] = first_name
+    result['نام خانوادگی'] = last_name
     phone_number = PhoneNumberDetection().find_phone_number(final_text)
-    print(f'Phone Number = {phone_number}')
+    result['شماره تماس'] = phone_number
     date = DateDetection().find_date_number(final_text)
-    print(f'Date = {date}')
+    result['تاریخ تولد'] = date
     extra = SectionExtractor().find_sections(final_text)
-    print(f'Extra = {extra}')
+    for info in extra:
+        if type(info) == dict:
+            result.update(info)
+    return result
--- cv_info_extractor/section_extractor.py
+++ cv_info_extractor/section_extractor.py
@@ -8,16 +8,16 @@ class SectionExtractor:
             self.keywords = [l.strip() for l in file.readlines()]
 
     def match_sections(self, inp):
-        idxs = [inp.find(k) for k in self.keywords if inp.find(k) != -1]
-        idxs.append(len(inp))
-        idxs.sort()
-        print(idxs)
-        matches = [inp[idxs[i]:idxs[i+1]] for i in range(len(idxs) - 1)]
-        print('These are matches: ', matches)
+        idxs = [{k:inp.find(k)} for k in self.keywords if inp.find(k) != -1]
+        idxs.append({'END':len(inp)})
+        idxs.sort(key=lambda x: list(x.values())[0])
+        idx_vals = [list(i.values())[0] for i in idxs]
+        matches = [{list(idxs[i].keys())[0]:inp[idx_vals[i] + len(list(idxs[i].keys())[0]) + 1:idx_vals[i+1]]} for i in range(len(idx_vals) - 1)]
+        for i in matches:
+            print(f'THIS IS THE MATCHED SECTION: {i}')
         return matches
 
     def find_sections(self, text):
-        
         matched_sections = self.match_sections(text)
         if not matched_sections:
             return 'Not Found'
