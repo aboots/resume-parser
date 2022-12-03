de3639ad35eb49b536a7cc3cc90869079da4260f
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2022-12-03 20:42:01 +0330

    Add Job title Extractor

---
+++ cv_info_extractor/job_title_extractor.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+class JobTitleFinder:
+    def __init__(self) -> None:
+        titles = list(
+            pd.read_csv(
+                "./resources/jobs.csv",
+            )["title"]
+        )
+        expanded_1 = [
+            text.replace("مهندس", "مهندسی") for text in titles if "مهندس" in text
+        ]
+        titles += expanded_1
+        self.titles = sorted(titles, key=lambda s: -len(s))
+        self.normalized_title = [self.normalize(text) for text in self.titles]
+
+    def normalize(self, txt):
+        txt = txt.replace(" ", "")
+        txt = txt.replace("\n", "")
+        txt = txt.replace("\u200C", "")
+        return txt
+
+    def find(self, text):
+        normalized_text = self.normalize(text)
+        min_index = float("inf")
+        for idx, title in enumerate(self.normalized_title):
+            if title in normalized_text:
+                index = normalized_text.index(title)
+                if index < min_index:
+                    return self.titles[idx]
