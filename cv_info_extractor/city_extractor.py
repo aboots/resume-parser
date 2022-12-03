c5787d1bb1e244c51f3ef2c1b374fa47b8f297d6
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2022-12-03 15:41:17 +0330

    Commit City

---
+++ cv_info_extractor/city_extractor.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import re
+
+
+class CityProvinceExtractor:
+    def __init__(self):
+        df = pd.read_csv("./resources/cities.csv")
+        self.cities = set(df["city"].to_list())
+        self.provinces = set(df["province"].to_list())
+        self.common_names = set.intersection(self.cities, self.provinces)
+
+    def find(self, original_text):
+        original_text = re.sub(r"[^\w\s]", "", original_text)
+        original_text = re.sub(r"\s+", " ", original_text)
+        original_text = re.sub(r"\n", " ", original_text)
+        text = original_text
+
+        for common_name in self.common_names:
+            text = text.replace(common_name, "#" * len(common_name))
+        for city in self.cities:
+            text = text.replace(city, "&" * len(city))
+        for province in self.provinces:
+            text = text.replace(province, "*" * len(province))
+
+        matches = []
+
+        for match in re.finditer(r" ([#|&]+) ([#|*]+) ", text):
+            city = original_text[match.span(1)[0] : match.span(1)[1]]
+            province = original_text[match.span(2)[0] : match.span(2)[1]]
+            text = (
+                text[: match.start()]
+                + "~" * (match.end() - match.start())
+                + text[match.end() :]
+            )
+            matches.append({"city": city, "province": province})
+        for match in re.finditer(r" ([#|&]+) استان ([#|*]+) ", text):
+            city = original_text[match.span(1)[0] : match.span(1)[1]]
+            province = original_text[match.span(2)[0] : match.span(2)[1]]
+            text = (
+                text[: match.start()]
+                + "~" * (match.end() - match.start())
+                + text[match.end() :]
+            )
+            matches.append({"city": city, "province": province})
+        for match in re.finditer(r" ([#|*]+) ", text):
+            province = original_text[match.span(1)[0] : match.span(1)[1]]
+            text = (
+                text[: match.start()]
+                + "~" * (match.end() - match.start())
+                + text[match.end() :]
+            )
+            matches.append({"city": None, "province": province})
+        return matches
